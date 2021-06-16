a42e746e5700554545e60324b50f984d604d1f8e
Carms Ng <contact@carms-ng.com>
2021-06-15 20:43:24 -0400

    [ADD][CMS] custom validation for modalText

--- studio/schemas/documents/announcement.js
+++ studio/schemas/documents/announcement.js
@@ -25,13 +25,23 @@ export default {
       name: 'linkText',
       title: 'Link text to Popup',
       type: 'localeString',
-      description: 'If you want a popup with more information about your announcement, what text would you like the user to click on to trigger the popup?',
+      description: 'Optional. If you want a popup with more information about your announcement, what text would you like the user to click on to trigger the popup?',
     },
     {
       name: 'modalText',
-      title: 'Popup Text',
+      title: 'Popup Message',
       type: 'localePortableText',
-      description: 'If you want a popup message, what text do you want to appear in the popup? Feel free to include links or anything you like.',
+      description: 'Required, when Link text to Popup is defined. This, along with the Title, will be displayed in the popup!',
+      validation: Rule => Rule.custom((field, context) => {
+        // implement popup if linkText is defined, validate that pop up message is defined
+        const isPopup = context.document.linkText[`en`] !== undefined
+
+        if (isPopup && field[`en`] === undefined) {
+          return "Link text to popup isn't empty, hence Popup message must not be empty."
+        } else {
+          return true
+        }
+      })
     },
   ],
   preview: {
